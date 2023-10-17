0ef99c5400b015fe26a1debcb5e58e2ed6c57aec
HAN JUA <69968801+Claire-art@users.noreply.github.com>
2023-10-18 07:28:47 +0900

    Create: Check1.cs

---
+++ Check1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Check1 : MonoBehaviour
+{
+    public static int key1 = 0;
+    public GameObject pass;
+    // public GameObject triggerObject;
+
+    public void check(InputField f) {
+        if (f.text == "물병자리")
+        {
+            key1 = 1;
+            print("정답");
+            pass.SetActive(false);
+            // Destroy(triggerObject);
+        }
+
+        else {
+            print("틀린 답");
+            pass.SetActive(true);
+        }
+    }
+}
